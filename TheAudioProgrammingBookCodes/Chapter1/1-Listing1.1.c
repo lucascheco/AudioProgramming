4bdbf6c1024372a56180494008b499abf062ed56
Lucas Pacheco <lmatheus.sp@gmail.com>
2020-05-07 20:51:20 -0300

    Newcodes
    New Codes for chapter 1

---
+++ TheAudioProgrammingBookCodes/Chapter1/1-Listing1.1.c
@@ -0,0 +1,17 @@
+//Author: Lucas Pacheco.
+//Description: A copy from "The Audio Programming Book", chapter 1, Listing 1.1.
+//Date: 07/06/2020.
+
+/* wonder.c:displays "What a Wonderful World!" in the terminal */
+#include <stdio.h>
+
+int main()
+{
+    printf("What a Wonderful World!\n");
+    return 0;
+}
+
+/*
+    OUTPUT SAMPLE:
+        What a Wonderful World!
+*/
