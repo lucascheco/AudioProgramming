6547e17d7ee98420d219742c4b00c2f599ee5993
Lucas Pacheco <lmatheus.sp@gmail.com>
2020-05-12 16:53:32 -0300

    Testing simple things
    Working with strings and pointers

---
+++ TheAudioProgrammingBookCodes/Teste/2-TestIndex.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+        char *name = "John";
+        name[2] = 'a';       /* This is illegal */
+
+    char name[] = "John";
+    name[2] = 'a';
+
+    printf("%s\n", name);
+
+    return 0;
+}
