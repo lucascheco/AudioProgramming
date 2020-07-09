dc3cca80fe78d9527b6e33ca155e21ae8e8ea55d
Lucas Pacheco <lmatheus.sp@gmail.com>
2020-07-09 17:51:46 -0300

    Create 7-Listing1.8.2.c

---
+++ TheAudioProgrammingBookCodes/Chapter1/7-Listing1.8.2.c
@@ -0,0 +1,24 @@
+//Author: Lucas Pacheco.
+//Description: Code  from "The Audio Programming Book", chapter 1, Listing1.8.2 .
+//Date: 09/07/2020.
+
+/* expbrk.c generate exponencial attack or decay breakpoint data */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char **argv)
+{
+    int i, npoints;
+    double startval, endval;
+    double dur, step, start, end, thisstep;
+    double fac, valrange, offset;
+    const double verysmall = 1.0e-4; /* -80dB */
+
+    if (argc != 5)
+    {
+        
+    }
+
+    return 0;
+}
