399cb7671de04f9154fa59d92fec5014eb2ccd0a
Lucas Pacheco <lmatheus.sp@gmail.com>
2020-07-09 18:48:41 -0300

    Update 7-Listing1.8.2.c

--- TheAudioProgrammingBookCodes/Chapter1/7-Listing1.8.2.c
+++ TheAudioProgrammingBookCodes/Chapter1/7-Listing1.8.2.c
@@ -17,8 +17,68 @@ int main(int argc, char **argv)
 
     if (argc != 5)
     {
-        
+        fprintf(stderr, "Usage: expbrk duration npoints startval endval\n");
+        return 1;
     }
+
+    dur = atof(argv[1]);
+
+    if (dur <= 0.0)
+    {
+        fprintf(stderr, "Error: duration must be positive.\n");
+        return 1;
+    }
+
+    npoints = atoi(argv[2]);
+
+    if (npoints <= 0)
+    {
+        fprintf(stderr, "Error: npoints must be positive!\n");
+        return 1;
+    }
+
+    step = dur / npoints;
+
+    startval = atof(argv[3]);
+    endval = atof(argv[4]);
+    valrange = endval - startval;
+    
+    if (valrange == 0.0)
+    {
+        fprintf(stderr, "warning: start and end values are the same!\n");
+    }
+
+    /* initialize normalized exponential as attack or decay */
+    if (startval > endval)
+    {
+        start = 1.0;
+        end = verysmall;
+        valrange = - valrange;
+        offset = endval;
+    }
+    else
+    {
+        startval = verysmall;
+        end = 1.0;
+        offset = startval;
+    }
+    
+    thisstep = 0.0;
+    
+    /* make normalized curve, scale output to input values, range */
+    fac = pow(end / start, 1.0 / npoints);
+
+    for (i = 0; i < npoints; i++)
+    {
+        fprintf(stdout, "%.4lf\t%.8lf\n", thisstep, offset + (start * valrange));
+        start *= fac;
+        thisstep += step;
+    }
+
+    /* print final value */
+    fprintf(stdout, "%.4lf\t%.8lf\n", thisstep, offset + (start * valrange));
+
+    fprintf(stderr, "done\n");
 
     return 0;
 }
