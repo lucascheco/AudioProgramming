1009341e7ddc66b93c692abfbf11037f8d3dbcd5
Lucas Pacheco <lmatheus.sp@gmail.com>
2020-07-13 01:14:51 -0300

    Edited

--- TheAudioProgrammingBookCodes/Chapter1/7-Listing1.8.2.c
+++ TheAudioProgrammingBookCodes/Chapter1/7-Listing1.8.2.c
@@ -56,7 +56,7 @@ int main(int argc, char **argv)
     {
         start = 1.0;
         end = verysmall;
-        valrange = - valrange;
+        valrange = -valrange;
         offset = endval;
     }
     else
