027ee8a43cf40af599ee2de81de90e40aa4e317f
lucascheco <lmatheus.sp@gmail.com>
2023-02-23 07:28:17 -0300

    Envelope synth

--- TheAudioProgrammingBookCodes/Chapter6/env_simplesynth.cpp
+++ TheAudioProgrammingBookCodes/Chapter6/env_simplesynth.cpp
@@ -8,37 +8,40 @@
 #include <stdlib.h>
 #include <snd_defs.h>
 
-/** Simple synthesis program.
-    Generates a sine-wave sound. This program shows the use of
-    truncating oscillators.
+/** simple synthesis program with envelopes.
+    Generates a saw-tooth sound with the frequency and amplitude
+    envelopes. This program also shows the use of interpolating oscillators.
 
-    oscillator sndfile.wav amp freq(Hz) dur(secs)
+    env sndfile.wav amp freq(Hz) dur(secs)
 */
 
 int main(int argc, char** argv) {
     SNDFILE *psf;
     float *buffer;
-    int dur;
+    int smps, cnt1=0, cnt2=0;
 
-    float amp, freq, *wave, cs, ndx=0, ndx2=0;
+    float dur, amp, freq, *wave, ndx=0;
 
     if (argc == 5) {
         amp = (float) atof(argv[2]);
         freq = (float) atof(argv[3]);
-        dur = (int) (atof(argv[4]) * def_cr);
+        dur = (int) atof(argv[4]);
+        smps = (int) (dur * def_cr);
 
         // allocate buffer and table memory
         buffer = new float[def_vsize];
         wave = sinus_table();
-
+        
         // now we open the file 
         if (!(psf = soundout_open(argv[1]))) {
             printf("error opening output file.\n");
             exit(-1);
         }
 
-        for (int i = 0; i < dur; i++) {
-            osc(buffer, amp, freq + osc(&cs, 10.f, 5.f, wave, &ndx2, def_len, 1,  def_cr), wave, &ndx);
+        for (int i = 0; i < smps; i++) {
+            oscc(buffer, amp * adsr(1.f, dur, 0.05f, 0.1f, 0.7f, 0.2f, &cnt1),
+                expon(freq, dur/2, freq*2, &cnt2),
+                wave, &ndx);
             soundout(psf, buffer);
         }
 
@@ -48,9 +51,8 @@ int main(int argc, char** argv) {
         delete[] wave;
 
         return 0;
-    }
-    else {
-        printf("usage: oscillator sndfile.wav "
+    } else {
+        printf("usage: envsndfile.wav "
                "amp freq(Hz) dur(s)\n");
         return 1;
     }
